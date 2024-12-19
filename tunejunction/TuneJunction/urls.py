85507511e2d0675d09130f48e7db65d712e7ac35
Venkateswararaobhimasingi <141164246+Venkateswararaobhimasingi@users.noreply.github.com>
2024-12-19 12:53:48 +0530

    Update urls.py

--- tunejunction/TuneJunction/urls.py
+++ tunejunction/TuneJunction/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('register/',user_views.register,name='register'),
     path('profile/',user_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('logout/',user_views.login,name='logout'),
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),name='password_reset'),
 
     path('password-reset/done',
